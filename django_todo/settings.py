50fff87830bdaef0531bc4259f35758920067a5b
rnc-personal <richard.challenger@protonmail.com>
2023-03-11 12:50:37 +0000

    changed db setup

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
-import env
+
+if os.path.isfile("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,7 +79,8 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': os.environ.get("DATABASE_URL")
+
 }
 
 
